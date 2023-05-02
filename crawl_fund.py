c7f56ff6e4b5938985c4c92673ba68caf5071384
centos <2233002@tongji.edu.com>
2023-05-02 23:46:17 +0800

    更新策略信息

--- crawl_fund.py
+++ crawl_fund.py
@@ -113,7 +113,6 @@ class Crawl(GetToken):
             ]
             return strategy_info
 
-    
     async def get_all_strategies(self, advisers_code: list):
         q = Queue()
         headers = {
@@ -162,6 +161,134 @@ class Crawl(GetToken):
                     os.makedirs(folder, exist_ok=True)
                 tg_name.extend(response)
         return tg_name
+
+    async def get_tg_extend_info(self, session: aiohttp.ClientSession, tg: dict):
+        url = 'https://uni-fundts.1234567.com.cn/merge/m/api/tgfund'
+        fields = "ACCOUNTID,MINEXPANNSYL,INVEST_TERM,EXPECT_MAX_RETREAT,MAXEXPANNSYL,SHTIME,JJGSID,VIDEO_IMAGE_URL,ENDTIME,STARTTIME,TARGETPROFIT,TARGETANNYIELD,ANNSYL_LN,SYL_D,SHOW_TYPE,SYRQ,HEAD_DISPLAY_TYPE,HEAD_FIELD1,FIELD1_EXPLAIN,HEAD_FIELD2,FIELD2_EXPLAIN,PERFORMANCE1_DISPLAY_TYPE,BENCHMARK_INDEX,PERFORMANCE2_DISPLAY_TYPE,IS_PREVIOUS_INCOME_DISPLAY,IS_TRADE_POSITION_SHOW,QUOTA_DISPLAY,PEOPLENUM_DISPLAY,FEATURE_EXPLAIN,LABEL1,LABEL2,LABEL3,LABEL4,RECOMMEND_HOLD_TIME,STRATEGY_CONCEPT1,STRATEGY_CONCEPT2,STRATEGY_CONCEPT3,IMG_URL,JUMP_TYPE,SELECT_TYPE,KEY_TITLE1,HOLD_LIMIT1,PARTNER,TGNAME,LOGO_NAME,LOGO_URL,BUY_CFM_DAYS,ESTABDATE,RISKLEVEL,STRATEGY_RATE,SYL_Z,BENCHSYL_Z,SYL_Y,BENCHSYL_Y,SYL_3Y,BENCHSYL_3Y,SYL_6Y,BENCHSYL_6Y,SYL_1N,BENCHSYL_1N,SYL_2N,BENCHSYL_2N,SYL_3N,BENCHSYL_3N,SYL_JN,BENCHSYL_JN,SYL_LN,BENCHSYL_LN,STYPE,BUYSTATUS,SERVICE_RATE_TYPE,STEP_RATE,STRATEGY_RATE_DISCOUNT,MINBUY,STGCONCEPT,FEATURE_EXPLAIN,OPERATETIME,OPERAENDTIME,PROVISION_TYPE,PROFIT_DAYS,REDEEM_CFM_DAYS,CASH_BAG_DAYS,CASH_DAYS,COLLECT_TYPE,SERVICE_DESC,BASIC_CAL_FORMULA_REMARK,SUPPORT_RATION,RUN_STATUS,END_STATUS,MAX_RUN_DATE,ACTUAL_RUN_DATE,STATUS,SAME_FUND"
+        data = {
+            'appVersion': self.app_version,
+            'ctoken': self.ctoken,
+            'deviceid': self.device_id,
+            'fields': fields,
+            'pageIndex': 1,
+            'pageSize': 3,
+            'passportctoken': self.passport_ctoken,
+            'passportid': self.passport_id,
+            'passportutoken': self.passport_utoken,
+            'plat': 'Iphone',
+            'product': 'Fund',
+            'serverversion': self.app_version,
+            'tgCode': tg['code'],
+            'userid': self.user_id,
+            'utoken': self.utoken,
+            'version': self.app_version
+        }
+        async with session.post(url, data=data) as response:
+            res = await response.read()
+            json_response = json.loads(res)
+            is_valid = (json_response['data'] is not None)
+            assert is_valid, f"<{tg['name']}>提供的<{tg['tg_name']}>策略暂无信息."
+            row_name_list = ['策略名称', '机构', '策略理念', '成立来收益', '日涨幅', '风险等级', '建议持有', '成立天数', '业绩比较基准']
+            row_value_list = []
+
+            data = json_response['data']
+            row_value_list.append(data['tgExtendInfo']['TGNAME'])
+            row_value_list.append(data['tgExtendInfo']['LOGO_NAME'])
+            # 处理策略理念
+            info_dict = dict(data['tgExtendInfo'])
+            concept1 = info_dict.get('STRATEGY_CONCEPT1')
+            if concept1 is None:
+                row_value_list.append(info_dict['STGCONCEPT'])
+            else:
+                concept_list = [concept1]
+                concept2 = info_dict.get('STRATEGY_CONCEPT2')
+                concept3 = info_dict.get('STRATEGY_CONCEPT3')
+                if concept2 is not None:
+                    concept_list.append(concept2)
+                if concept3 is not None:
+                    concept_list.append(concept3)
+                tg_concept = '; '.join(concept_list)
+                row_value_list.append(tg_concept)
+            # 处理成立来收益
+            row_value_list.append(data['tgExtendInfo']['SYL_LN'] + '%')
+            # 处理日涨幅
+            row_value_list.append(f"{data['tgExtendInfo']['SYRQ']}, ratio: {data['tgExtendInfo']['SYL_D']}%")
+            # 处理风险等级
+            risk_level = {
+                '1': '低风险',
+                '2': '中低风险',
+                '3': '中风险',
+                '4': '中高风险',
+                '5': '高风险'
+            }
+            row_value_list.append(risk_level[data['tgExtendInfo']['RISKLEVEL']])
+            # 处理建议持有
+            if data['tgExtendInfo']['RECOMMEND_HOLD_TIME'] is not None:
+                row_value_list.append(data['tgExtendInfo']['RECOMMEND_HOLD_TIME'])
+            else:
+                row_value_list.append('--')
+            # 处理成立天数
+            row_value_list.append(str(data['tgExtendInfo']['continuedData']) + '天')
+            # 处理业绩比较标准
+            row_value_list.append(data['tgExtendInfo']['BASIC_CAL_FORMULA_REMARK'])
+            # 处理特殊数据
+            ch_data = {
+                "user_POSITIVE_RATIO": "用户持仓正收益占比",
+                "user_FGRATIO": "策略复购率",
+                "user_AVGDAY": "用户平均持有时长",
+                "user_STAY_RATIO": "近一年留存率",
+                "excess_LN": "成立来超额收益",
+                "maxretra_LN": "成立来最大回撤",
+                "maxretra_1N": "近一年最大回撤",
+                "mexwin_1N": "近一年跑赢基准概率",
+                "profit_1Y": "持有一个月盈利概率",
+                "profit_3Y": "持有三个月盈利概率",
+                "profit_6Y": "持有六个月盈利概率",
+                "profit_1N": "持有一年盈利概率",
+            }
+            tg_characteristics_page2 = dict(data['tgCharacteristics']['tgCharacteristicsPage2'])
+            for key, value in tg_characteristics_page2.items():
+                if key in ["user_POSITIVE_RATIO", "user_FGRATIO", "user_AVGDAY", "user_STAY_RATIO"]:
+                    row_name_list.append(ch_data[key])
+                    if key == "user_AVGDAY":
+                        row_value_list.append(str(value) + '天')
+                    else:
+                        row_value_list.append(str(value) + '%')
+            for key, value in tg_characteristics_page2.items():
+                if key in ["excess_LN", "maxretra_LN", "maxretra_1N", "mexwin_1N"]:
+                    row_name_list.append(ch_data[key])
+                    row_value_list.append(str(value) + '%')
+            for key, value in tg_characteristics_page2.items():
+                if key in ["profit_1Y", "profit_3Y", "profit_6Y", "profit_1N"]:
+                    row_name_list.append(ch_data[key])
+                    row_value_list.append(str(value) + '%')
+
+            folder = os.path.join(self.folder_name, tg['name'], tg['tg_name'])
+
+            return {'data': {'info': row_name_list, 'value': row_value_list}, 'folder': folder}
+
+    async def save_tg_extend_info(self, tg_name: list):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Referer': 'https://mpservice.com/funda91a99886abf7e/release/pages/strategyDetail/index',
+            'clientInfo': 'ttjj-iPhone 13 Pro-iOS-iOS16.4.1',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-Hans-CN;q=1, en-CN;q=0.9, de-CN;q=0.8',
+            'Connection': 'keep-alive',
+            'User-Agent': 'EMProjJijin/6.6.4 (iPhone; iOS 16.4.1; Scale/3.00)',
+            'GTOKEN': self.gtoken
+        }
+        async with aiohttp.ClientSession(headers=headers) as session:
+            fetchers = [self.get_tg_extend_info(session, tg) for tg in tg_name]
+            for finished_task in asyncio.as_completed(fetchers):
+                try:
+                    response = await finished_task
+                    save_to_csv(f"{response['folder']}/basic_info.csv",
+                                info=response['data']['info'],
+                                value=response['data']['value'],)
+                except Exception as e:
+                    print(f"Warn:{e}")
 
     async def get_all_netval(self, tg_name: list):
         headers = {
@@ -247,7 +374,7 @@ class Crawl(GetToken):
                 netVal.append(item['SE'] + '%')
                 bench_netVal.append(item['BENCH_SE'] + '%')
 
-            return {'is_valid': is_valid, 'data': {'date': date, 'netVal': netVal, 'bench_netVal': bench_netVal}, 'folder': folder}
+            return {'data': {'date': date, 'netVal': netVal, 'bench_netVal': bench_netVal}, 'folder': folder}
 
     async def get_warehouse(self, session: aiohttp.ClientSession, tg: dict):
 
@@ -318,6 +445,7 @@ class Crawl(GetToken):
 
         asyncio.run(self.get_all_netval(all_strategy))
         asyncio.run(self.get_all_warehouse(all_strategy))
+        asyncio.run(self.save_tg_extend_info(all_strategy))
 
     def run_in_new_loop_1(self, all_strategy):
         asyncio.run(self.get_all_netval(all_strategy))
@@ -326,8 +454,8 @@ class Crawl(GetToken):
         asyncio.run(self.get_all_warehouse(all_strategy))
 
 
-# startTime = time.time()
-# c = Crawl()
-# c.run()
-# endTime = time.time()
-# print("基金数据更新完毕", endTime - startTime)
+startTime = time.time()
+c = Crawl()
+c.run()
+endTime = time.time()
+print("基金数据更新完毕", endTime - startTime)
